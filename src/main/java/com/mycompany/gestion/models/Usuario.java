ca9d69da841217b5facb05ca9422232e6957c201
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-09-09 19:52:39 -0300

    Agregar formulario para ABM usuarios

---
+++ src/main/java/com/mycompany/gestion/models/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestion.models;
+
+/**
+ *
+ * @author Pc
+ */
+public class Usuario {
+    
+}
