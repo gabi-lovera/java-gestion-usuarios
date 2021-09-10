4a311f6e807e7e077a64a053fb305a52f8bdad7d
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-09-10 08:12:04 -0300

    Modelo usuario getters y setters

--- src/main/java/com/mycompany/gestion/models/Usuario.java
+++ src/main/java/com/mycompany/gestion/models/Usuario.java
@@ -7,8 +7,58 @@ package com.mycompany.gestion.models;
 
 /**
  *
- * @author Pc
+ * @author gabriel
  */
 public class Usuario {
+    
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
     
 }
